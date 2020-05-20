de10e263c2fc060e2017af66cb441ce89ce65f3b
PH Le <phd.levn@gmail.com>
2020-05-21 00:08:16 +0100

    chore: clear the gpu models

--- Project/train/train.py
+++ Project/train/train.py
@@ -67,9 +67,33 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     device       - Where the model and data should be loaded (gpu or cpu).
     """
     
-    # TODO: Paste the train() method developed in the notebook here.
+    counter = 0
+    clip=5 # gradient clipping
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:         
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
 
-    pass
+            # zero accumulated gradients
+            model.zero_grad()
+            
+            # get the output from the model
+            output = model(batch_X)
+
+            # calculate the loss and perform backprop
+            loss = loss_fn(output, batch_y)
+            loss.backward()
+            # `clip_grad_norm` helps prevent the exploding gradient problem in RNNs / LSTMs.
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip)
+            optimizer.step()
+
+
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
 
 
 if __name__ == '__main__':
